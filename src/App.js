a640451b9fae8fe384a49007706a314699e1a16a
tpinto01 <pintoleitetomas@gmail.com>
2021-08-22 20:08:14 -0400

    First commit, Task Tracker, react crash course, components, props, hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30 PM',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Business Meeting',
+      day: 'Feb 6th at 1:30 PM',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 4:30 PM',
+      reminder: false,
+    },
+  ])
+
+
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header /> 
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
